e539199c332d84c1a0baaa951532c3a6292a6b5e
Shiva <shivakumarsunderamurthy@gmail.com>
2022-09-08 10:13:32 +0800

    Webpack vue django

---
+++ weather/core/views.py
@@ -0,0 +1,4 @@
+from django.views.generic.base import TemplateView
+
+class IndexTemplateView(TemplateView):
+    template_name = 'index.html'
--- weather/frontend/vue.config.js
+++ weather/frontend/vue.config.js
@@ -1,4 +1,27 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  configureWebpack: {
+    plugins: [new MiniCssExtractPlugin()],
+  },
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/static/dist/"
+      : "http://127.0.0.1:8080",
+  outputDir: "../static/dist",
+  indexPath: "../../templates/index.html",
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "weather",
+    },
+  },
+  devServer: {
+    devMiddleware: {
+      publicPath: "http://127.0.0.1:8080",
+      writeToDisk: (filePath) => filePath.endsWith("index.html"),
+    },
+    hot: "only",
+    headers: { "Access-Control-Allow-Origin": "*" },
+  },
+};
